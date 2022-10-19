f79b648f83ac4586357e2c124fca60c16a5e5f62
wahid-ari <wahiid.arii@Gmail.com>
2022-10-19 16:38:44 +0700

    update API_URL

--- next.config.js
+++ next.config.js
@@ -1,12 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env : {
-    API_URL: "http://localhost:3000",
-    // API_URL: "",
+  env: {
+    // API_URL: "http://localhost:3000",
+    API_URL: "https://next-og-images.vercel.app",
   },
   reactStrictMode: true,
   images: {
-    domains: ["images.unsplash.com", "http://localhost", "localhost"],
+    domains: [
+      "images.unsplash.com",
+      "http://localhost",
+      "localhost",
+      "https://next-og-images.vercel.app",
+      "next-og-images.vercel.app"
+    ],
   },
 }
 
